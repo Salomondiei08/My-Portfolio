c09b8a6ce121f973f3c9501a212750cdf0e8794e
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-03-04 21:49:44 +0000

    Added the Title, Bio, Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Avatar from "./components/Avatar";
 import Name from "./components/Name";
+import Title from "./components/Title";
+import Bio from "./components/Bio";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
       <Avatar />
       <Name fname="Matthew" lname="Young" />
+      <Title />
+      <Bio />
+      <Button />
     </>
   );
 }
