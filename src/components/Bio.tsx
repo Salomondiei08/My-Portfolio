bf108e25d78590e768bc73985267377e2ee809b1
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-03-04 20:16:10 +0000

    feat: Added bio component

---
+++ src/components/Bio.tsx
@@ -0,0 +1,16 @@
+const Bio = () => {
+  return (
+    <>
+      <div className="mx-auto flex justify-center gap-2 mt-2">
+        <p className="text-gray-400 text-lg text-center mt-3 px-3">
+          I'm a Front-End Developer — a{" "}
+          <span className="text-white">builder </span>
+          focused on creating user-friendly products and experiences for the
+          web.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Bio;
