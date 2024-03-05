bc2577e49c04a8d2cb5f6e0b54867be3fb79b320
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-03-05 15:52:42 +0000

    fix: made btn components responsive on lg screens

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,9 +3,9 @@ import { IoMailSharp } from "react-icons/io5";
 const Button = () => {
   return (
     <div className="mx-auto flex justify-center mt-7">
-      <button className="flex bg-white px-20 py-2 rounded-md">
-        <IoMailSharp />
-        Get in touch
+      <button className="flex bg-white px-16 py-2 sm:px-24 md:px-36 rounded-lg text-lg">
+        Get In Touch
+        <IoMailSharp className="mt-0 ml-2 size-7" />
       </button>
     </div>
   );
