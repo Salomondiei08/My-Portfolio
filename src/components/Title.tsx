af18e40602ac635f6de4baaeab05588de1bf2d82
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-03-05 15:49:45 +0000

    fix: Made title responsive

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,9 @@
 const Title = () => {
   return (
-    <div className="mx-auto flex justify-center gap-2 mt-2">
-      <p className="text-white text-center text-lg font-bold align-middle">
-        Front-End Developer
-      </p>
-      <p className="text-gray-400 text-center text-lg"> • </p>
-      <p className="text-gray-400 text-center text-lg align-middle">
+    <div className="mx-auto flex justify-center gap-1 mt-2">
+      <p className="text-white text-center text-nowrap">Front-End Developer</p>
+      <p className="text-gray-400 text-center text-base"> • </p>
+      <p className="text-gray-400 text-center text-nowrap text-base align-middle">
         Abidjan, Côte d'Ivoire
       </p>
     </div>
