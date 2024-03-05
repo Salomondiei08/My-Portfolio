4304b17eb108167fad40e2a8a0dc12e8a2010df7
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-03-05 16:54:38 +0000

    style: added hover effect

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { IoMailSharp } from "react-icons/io5";
 const Button = () => {
   return (
     <div className="mx-auto flex justify-center mt-7">
-      <button className="flex bg-white px-16 py-2 sm:px-24 md:px-36 rounded-lg text-lg">
+      <button className="flex bg-white hover:bg-neutral-200 px-16 py-2 sm:px-24 md:px-36 rounded-lg text-lg">
         Get In Touch
         <IoMailSharp className="mt-0 ml-2 size-7" />
       </button>
