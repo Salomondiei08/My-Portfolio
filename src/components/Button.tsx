b8062e4e9c6812edd06ffeced13f9a38dbcbac11
Salomon Diei <salomondiei08@gmail.com>
2024-06-18 11:52:25 +0000

    chore: change email

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { IoMailSharp } from "react-icons/io5";
 const Button = () => {
   return (
     <div className="mx-auto flex justify-center mt-7">
-      <a href="mailto:mattyoungjnr@gmail.com" rel="noreferrer" target="_blank">
+      <a href="mailto:salomon@reinvent-labs.com" rel="noreferrer" target="_blank">
         <button className="flex bg-white hover:bg-neutral-200 px-16 py-2 sm:px-24 md:px-36 rounded-lg text-lg">
           Get In Touch
           <IoMailSharp className="mt-0 ml-2 size-7" />
