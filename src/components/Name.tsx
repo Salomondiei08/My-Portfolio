867efb3c85c0e4ae5770b4bed1d5b7db1521f120
Salomon Diei <salomondiei08@gmail.com>
2024-06-18 12:29:50 +0000

    fix: fiix h2 tag bug

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -9,9 +9,9 @@ const Name = ({ fname, lname }: NameProps) => {
       <div className="container mx-auto w-full flex justify-center mt-6 text-center">
         <h2 className="text-white text-3xl font-sans font-normal">
           {fname + " " + lname}
-          <h2 className="text-gray-400 sm:text-2xl font-sans font-normal">
-            {"@TheReinventGuy"}
-          </h2>
+          <span className="text-gray-400 sm:text-2xl font-sans font-normal">
+            {" @TheReinventGuy"}
+          </span>
         </h2>
       </div>
     </>
